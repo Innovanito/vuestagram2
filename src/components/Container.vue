db6abac849ef78815d18bcdd1141865c2e99ef25
Innovanito <khg05311@naver.com>
2021-11-12 17:06:17 +0900

    2강 끝

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :postData="post" v-for="post  in postData" :key="post"   />
+    <!-- <Post :postData="postData[0]" /> -->
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+  components: {
+    Post
+  },
+  props: {
+    postData : Array
+  }
+
+}
+</script>
+
+<style>
+
+</style>
